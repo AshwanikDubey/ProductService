5583473f06343e9d1b34f969e343631d0aabf144
AshwanikDubey <ashwanidubey.work@gmail.com>
2025-10-04 08:19:58 +0530

    Product Controller Message

--- src/main/java/com/example/productservice/controllers/ProductController.java
+++ src/main/java/com/example/productservice/controllers/ProductController.java
@@ -1,4 +1,12 @@
 package com.example.productservice.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
+    @RequestMapping
+    public String productController(){
+        return "Product Controller";
+    }
 }
